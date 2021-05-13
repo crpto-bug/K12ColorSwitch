885a881184a249d903384e64e9ef31770afa932a
crpto-bug <mehaboob097@gmail.com>
2021-05-13 16:38:52 +0530

    Desktop Version

---
+++ K12 Colour Switch/Assets/Rotate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 100f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+    }
+}
